453e48d56aadd9ef51fec9a71cf5fcc275be3227
kange88 <cerisetal@naver.com>
2018-04-02 14:40:44 +0900

    2018-04-02
    Yesmember add

---
+++ src/com/DDot/model/YesMember.java
@@ -0,0 +1,14 @@
+package com.DDot.model;
+
+public class YesMember {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
